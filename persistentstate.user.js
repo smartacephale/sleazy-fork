37db8157825d21646ad73bc869fafe8099ba091b
mormon-atm <atm.mormon@protonmail.com>
2024-05-05 04:58:14 +0300

    Create persistentstate.user.js

---
+++ persistentstate.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         persistentstate
+// @description  simple state manager based on Vue3.reactive and localStorage
+// @namespace    http://tampermonkey.net/
+// @author       smartacephale
+// @license      MIT
+// @version      1.0
+// @match        *://*/*
+// ==/UserScript==
+/* globals reactive, watch */
+
+class PersistentState {
+    key = "state_2";
+
+    constructor(state) {
+        this.state = reactive(state);
+        this.sync();
+        this.watchPersistence();
+    }
+
+    sync() {
+        this.trySetFromLocalStorage();
+        window.addEventListener('focus', this.trySetFromLocalStorage);
+    }
+
+    watchPersistence() {
+        watch(this.state, (value) => {
+            this.saveToLocalStorage(this.key, value);
+        });
+    }
+
+    saveToLocalStorage(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    trySetFromLocalStorage = () => {
+        const localStorageValue = localStorage.getItem(this.key);
+        if (localStorageValue !== null) {
+            const prevState = JSON.parse(localStorageValue);
+            for (const prop of Object.keys(prevState)) {
+                this.state[prop] = prevState[prop];
+            }
+        }
+    }
+}
