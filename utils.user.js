6ebca99f56f06a16d26556dfbb4d1ab4c4ece54a
mormon-atm <atm.mormon@protonmail.com>
2024-05-05 04:33:54 +0300

    Update utils.user.js

--- utils.user.js
+++ utils.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         utils
-// @description  helper functions
+// @description  helper functions, intersection observer, mutation observer
 // @namespace    http://tampermonkey.net/
 // @author       smartacephale
 // @license      MIT
-// @version      1.0.2
+// @version      1.0.3
 // @match        *://*/*
 // ==/UserScript==
 
@@ -28,7 +28,67 @@ function parseIntegerOr(n, or) {
     return Number.isInteger(parseInt(n)) ? parseInt(n) : or;
 }
 
-function stringToTags(s) {
+function stringToWords(s) {
     return s.split(",").map(s => s.trim().toLowerCase()).filter(_ => _);
+}
+
+function parseCSSUrl(s) { return s.replace(/url\("|\"\).*/g, ''); }
+
+function circularShift(n, c = 6, s = 1) { return (n + s) % c || c; }
+
+function range(size, startAt = 1) {
+    return [...Array(size).keys()].map(i => i + startAt);
+}
+
+function listenEvents(dom, events, callback) {
+    for (const e of events) {
+        dom.addEventListener(e, callback, true);
+    }
+}
+
+//====================================================================================================
+
+class Observer {
+    constructor(callback) {
+        this.callback = callback;
+        this.observer = new IntersectionObserver(this.handleIntersection.bind(this));
+    }
+
+    observe(target) {
+        this.observer.observe(target);
+    }
+
+    throttle(target, throttleTime) {
+        this.observer.unobserve(target);
+        setTimeout(() => this.observer.observe(target), throttleTime);
+    }
+
+    handleIntersection(entries) {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                this.callback(entry.target);
+            }
+        }
+    }
+
+    static observeWhile(target, callback, throttleTime) {
+        const observer_ = new Observer(async (target) => {
+            const condition = await callback();
+            if (condition) observer_.throttle(target, throttleTime);
+        });
+        observer_.observe(target);
+        return observer_;
+    }
+}
+
+//====================================================================================================
+
+function waitForElementExists(parent, selector, callback) {
+    const observer = new MutationObserver((mutations) => {
+        const el = parent.querySelector(selector);
+        if (el) callback(el);
+        observer.disconnect();
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
 }
 
