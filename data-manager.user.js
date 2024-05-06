e2ff5ad364021656f5e9ac8c26e06428a650549e
smartacephale <atm.mormon@protonmail.com>
2024-05-06 23:49:50 +0300

    Update data-manager.user.js

--- data-manager.user.js
+++ data-manager.user.js
@@ -129,7 +129,7 @@ class DataManager {
     this.filter_(applyFilters, offset);
   }
 
-  handleLoadedHTML = (html, container, doNotAppend = false) => {
+  handleLoadedHTML = (html, container) => {
     const thumbs = this.rules.GET_THUMBS(html);
 
     const data_offset = this.data.size;
@@ -145,7 +145,6 @@ class DataManager {
       const { img, imgSrc } = this.rules.THUMB_IMG_DATA(thumbElement);
       this.lazyImgLoader.lazify(thumbElement, img, imgSrc);
 
-      if (doNotAppend === true) continue;
       (container || this.rules.CONTAINER).appendChild(thumbElement);
     }
 
