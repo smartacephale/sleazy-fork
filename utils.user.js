53e4f826b926500f25bef64b254b7fab8526428d
smartacephale <atm.mormon@protonmail.com>
2024-05-09 09:02:21 +0300

    waitForElementExists

--- utils.user.js
+++ utils.user.js
@@ -4,7 +4,7 @@
 // @namespace    http://tampermonkey.net/
 // @author       smartacephale
 // @license      MIT
-// @version      1.3
+// @version      1.3.1
 // @match        *://*/*
 // ==/UserScript==
 
