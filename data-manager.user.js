81b4c306575fdd438999dea61dade787752686d1
smartacephale <atm.mormon@protonmail.com>
2024-05-12 00:43:05 +0300

    Update data-manager.user.js

--- data-manager.user.js
+++ data-manager.user.js
@@ -4,10 +4,10 @@
 // @namespace    http://tampermonkey.net/
 // @author       smartacephale
 // @license      MIT
-// @version      1.0
+// @version      1.1
 // @match        *://*/*
 // ==/UserScript==
-/* globals */
+/* globals LazyImgLoader */
 
 /** Manages thumbs, applying filters, lazy loading, keep list unique */
 class DataManager {
@@ -39,30 +39,30 @@ class DataManager {
   }
 
   dataFilters = {
-    filterPublic: new class {
-      tag = 'filtered-public';
-      createFilter = () => {
-        return (v) => [this.tag, !this.rules.IS_PRIVATE(v.element) && this.state.filterPublic]
+    filterPublic: {
+      tag: 'filtered-public',
+      createFilter() {
+        return (v) => [this.tag, !this.rules.IS_PRIVATE(v.element) && this.state.filterPublic];
       }
     },
-    filterPrivate: new class {
-      tag = 'filtered-private';
-      createFilter = () => {
-        return (v) => [this.tag, this.rules.IS_PRIVATE(v.element) && this.state.filterPrivate]
+    filterPrivate: {
+      tag: 'filtered-private',
+      createFilter(){
+        return (v) => [this.tag, this.rules.IS_PRIVATE(v.element) && this.state.filterPrivate];
       }
     },
-    filterDuration: new class {
-      tag = 'filtered-duration';
-      createFilter = () => {
+    filterDuration: {
+      tag: 'filtered-duration',
+      createFilter() {
         return (v) => {
           const notInRange = v.duration < this.state.filterDurationFrom || v.duration > this.state.filterDurationTo;
           return [this.tag, this.state.filterDuration && notInRange];
         }
       }
     },
-    filterExclude: new class {
-      tag = 'filtered-exclude';
-      createFilter = () => {
+    filterExclude: {
+      tag: 'filtered-exclude',
+      createFilter() {
         const tags = stringToWords(this.state.filterExcludeWords);
         return (v) => {
           const containTags = tags.some(tag => v.title.includes(tag));
@@ -70,9 +70,9 @@ class DataManager {
         }
       }
     },
-    filterInclude: new class {
-      tag = 'filtered-include';
-      createFilter = () => {
+    filterInclude: {
+      tag: 'filtered-include',
+      createFilter() {
         const tags = stringToWords(this.state.filterIncludeWords);
         return (v) => {
           const containTagsNot = tags.some(tag => !v.title.includes(tag));
@@ -83,18 +83,16 @@ class DataManager {
   }
 
   setupFilters() {
-    // select through state jfc it's just wrong but fuck it fuck my ass 
+    // select through state jfc it's just wrong but fuck it fuck my ass
     Object.keys(this.dataFilters).forEach(k => {
       if (!Object.hasOwn(this.state, k)) {
         delete this.dataFilters[k];
       }
     });
 
-    // add filters style
     const tags = Object.keys(this.dataFilters).map(k => `.${this.dataFilters[k].tag}`).join(',');
     GM_addStyle(`${tags} { display: none !important; }`);
 
-    // bind state
     Object.values(this.dataFilters).forEach(f => {
       f.state = this.state;
       f.rules = this.rules;
@@ -131,7 +129,6 @@ class DataManager {
 
   handleLoadedHTML = (html, container) => {
     const thumbs = this.rules.GET_THUMBS(html);
-
     const data_offset = this.data.size;
 
     for (const thumbElement of thumbs) {
@@ -139,7 +136,6 @@ class DataManager {
       if (!url || this.data.has(url)) continue;
 
       const { title, duration } = this.rules.THUMB_DATA(thumbElement);
-
       this.data.set(url, { element: thumbElement, duration, title });
 
       const { img, imgSrc } = this.rules.THUMB_IMG_DATA(thumbElement);
@@ -147,8 +143,6 @@ class DataManager {
 
        const parent = container || this.rules.CONTAINER;
        if (!parent.contains(thumbElement)) parent.appendChild(thumbElement);
-      // fuck knows what to except...
-      // (container || this.rules.CONTAINER).appendChild(thumbElement);
     }
 
     this.filterAll(data_offset);
