49de8f01be595faae4df9e362ccfc915d55c77ab
smartacephale <atm.mormon@protonmail.com>
2024-05-18 01:41:30 +0300

    Update utils.user.js

--- utils.user.js
+++ utils.user.js
@@ -4,7 +4,7 @@
 // @namespace    http://tampermonkey.net/
 // @author       smartacephale
 // @license      MIT
-// @version      1.4.7
+// @version      1.4.8
 // @match        *://*/*
 // @downloadURL https://update.greasyfork.org/scripts/494206/utils.user.js
 // @updateURL https://update.greasyfork.org/scripts/494206/utils.meta.js
