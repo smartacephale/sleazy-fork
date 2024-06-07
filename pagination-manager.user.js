84e6445f9020b293e1665b0c7faf8359853aa48d
smartacephale <atm.mormon@protonmail.com>
2024-06-07 14:19:01 +0300

    fixes, async-generator option

--- pagination-manager.user.js
+++ pagination-manager.user.js
@@ -4,13 +4,15 @@
 // @namespace    http://tampermonkey.net/
 // @author       smartacephale
 // @license      MIT
-// @version      1.0
+// @version      1.1
 // @match        *://*/*
+// @downloadURL https://update.greasyfork.org/scripts/494205/pagination-manager.user.js
+// @updateURL https://update.greasyfork.org/scripts/494205/pagination-manager.meta.js
 // ==/UserScript==
-/* globals Observer */
+/* globals Observer fetchHtml */
 
 class PaginationManager {
-  /**
+    /**
  * @param {Vue.reactive} state
  * @param {Vue.reactive} stateLocale
  * @param {Rules} rules - WEBSITE_RULES class which have to implement methods:
@@ -19,47 +21,47 @@ class PaginationManager {
  * @param {Function} handleHtmlCallback
  * @param {number} delay - milliseconds
  */
-  constructor(state, stateLocale, rules, handleHtmlCallback, delay) {
-    this.state = state;
-    this.stateLocale = stateLocale;
-    this.rules = rules;
-    this.handleHtmlCallback = handleHtmlCallback;
+    constructor(state, stateLocale, rules, handleHtmlCallback, delay) {
+        this.state = state;
+        this.stateLocale = stateLocale;
+        this.rules = rules;
+        this.handleHtmlCallback = handleHtmlCallback;
 
-    this.stateLocale.pagIndexLast = this.rules.PAGINATION_LAST;
-    this.paginationGenerator = this.createNextPageGenerator();
-    this.paginationObserver = Observer.observeWhile(RULES.INTERSECTION_OBSERVABLE || 
-                                                    RULES.PAGINATION, this.generatorConsume, delay);
-  }
-
-  generatorConsume = async () => {
-    if (!this.state.infiniteScrollEnabled) return;
-    const {
-      value: { url, offset } = {},
-      done,
-    } = this.paginationGenerator.next();
-    if (!done) {
-      console.log(url);
-      const nextPageHTML = await fetchHtml(url);
-      const prevScrollPos = document.documentElement.scrollTop;
-      this.handleHtmlCallback(nextPageHTML);
-      this.stateLocale.pagIndexCur = offset;
-      window.scrollTo(0, prevScrollPos);
+        this.stateLocale.pagIndexLast = this.rules.PAGINATION_LAST;
+        this.paginationGenerator = this.createNextPageGenerator();
+        this.isPaginationGeneratorAsync = /AsyncGenerator/.test(Object.prototype.toString.call(this.paginationGenerator));
+        this.paginationObserver = Observer.observeWhile(this.rules.INTERSECTION_OBSERVABLE ||
+                                                        this.rules.PAGINATION, this.generatorConsume, delay);
     }
-    return !this.generatorDone;
-  }
 
-  createNextPageGenerator() {
-    const { offset, iteratable_url } = this.rules.URL_DATA();
-    const offsetLast = this.rules.PAGINATION_LAST;
-    this.stateLocale.pagIndexCur = offset;
-    function* nextPageGenerator() {
-      for (let c = offset + 1; c <= offsetLast; c++) {
-        const url = iteratable_url(c);
-        yield { url, offset: c };
-      }
+    generatorConsume = async () => {
+        if (!this.state.infiniteScrollEnabled) return;
+        const {
+            value: { url, offset } = {},
+            done,
+        } = (this.isPaginationGeneratorAsync ?
+             await this.paginationGenerator.next() : this.paginationGenerator.next());
+        if (!done) {
+            console.log(url);
+            const nextPageHTML = await fetchHtml(url);
+            const prevScrollPos = document.documentElement.scrollTop;
+            this.handleHtmlCallback(nextPageHTML);
+            this.stateLocale.pagIndexCur = offset;
+            window.scrollTo(0, prevScrollPos);
+        }
+        return !this.generatorDone;
     }
-    return nextPageGenerator();
-  }
+
+    createNextPageGenerator() {
+        const { offset, iteratable_url } = this.rules.URL_DATA();
+        const offsetLast = this.rules.PAGINATION_LAST;
+        this.stateLocale.pagIndexCur = offset;
+        function* nextPageGenerator() {
+            for (let c = offset + 1; c <= offsetLast; c++) {
+                const url = iteratable_url(c);
+                yield { url, offset: c };
+            }
+        }
+        return nextPageGenerator();
+    }
 }
-
-
