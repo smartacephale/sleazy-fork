6d98e6584156171895b08ba3162c06fd203ca415
smartacephale <atm.mormon@protonmail.com>
2024-06-07 14:47:44 +0300

    Create lskdb.user.js

---
+++ utils/lskdb.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         lskdb
+// @description  locale storage key database
+// @namespace    http://tampermonkey.net/
+// @author       smartacephale
+// @license      MIT
+// @version      1.0
+// @match        *://*/*
+// ==/UserScript==
+
+/*
+  raison d'etre for this insane degenerative garbage:
+  if you have to store 1000+ unique keys, 
+  you don't have to parse whole json array to check one value
+*/
+
+class LSKDB {
+    lockKey = 'lsmngr-lock';
+    prefix = 'lsm-';
+
+    constructor() {
+        // migration
+        const old = localStorage.getItem('lsmngr');
+        if (!old) return;
+        const list = JSON.parse(old);
+        localStorage.removeItem('lsmngr');
+        list.forEach(l => {
+            const i = localStorage.getItem(l);
+            if (i) {
+                const v = JSON.parse(i);
+                v.forEach(x => this.setKey(x));
+            }
+            localStorage.removeItem(l);
+        });
+    }
+
+    getKeys(n = 12) {
+        const res = [];
+        for (const key in localStorage) {
+            if (res.length >= n) break;
+            if (key.startsWith(this.prefix)) {
+                res.push(key);
+            }
+        }
+        res.forEach(k => localStorage.removeItem(k));
+        return res.map(r => r.slice(this.prefix.length));
+    }
+
+    hasKey(key) {
+        return localStorage.hasOwnProperty(`${this.prefix}${key}`);
+    }
+
+    setKey(key) {
+        localStorage.setItem(`${this.prefix}${key}`, '');
+    }
+
+    isLocked() {
+        const lock = localStorage.getItem(this.lockKey);
+        const locktime = 5*60*1000;
+        return !(!lock || Date.now() - lock > locktime);
+    }
+
+    lock(value) {
+        if (value) {
+            localStorage.setItem(this.lockKey, Date.now());
+        } else {
+            localStorage.removeItem(this.lockKey);
+        }
+    }
+}
