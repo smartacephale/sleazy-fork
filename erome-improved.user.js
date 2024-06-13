a90b19a5fb232a5e5fae3ad46211fce0411759ea
smartacephale <atm.mormon@protonmail.com>
2024-06-13 15:25:58 +0300

    Update erome-improved.user.js

--- erome-improved.user.js
+++ erome-improved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Erome Improved
 // @namespace    http://tampermonkey.net/
-// @version      2.2.1
+// @version      2.2.2
 // @license      MIT
 // @description  Infinite scroll. Filter photo albums. Filter photos in albums. Skips 18+ dialog
 // @author       smartacephale
@@ -13,7 +13,7 @@
 // @downloadURL  https://update.sleazyfork.org/scripts/492883/Erome%20Improved.user.js
 // @updateURL    https://update.sleazyfork.org/scripts/492883/Erome%20Improved.meta.js
 // ==/UserScript==
-/* globals jQuery, $, LazyLoad */
+/* globals $ LazyLoad */
 
 const LOGO = `
 ⡝⣝⢝⢝⢝⢝⢝⢝⢍⠭⡩⢍⠭⡩⡍⡭⢍⠭⡍⡭⡙⡍⢏⢝⠩⡩⡋⡍⡏⡝⡝⡽⡹⡹⡹⡙⡝⡙⡝⢍⠭⡩⢍⠭⡩⡩⡩⡩⡩⡍⡭⡩⡍⣍⢫⡩⡹⡩⡹⡩
@@ -51,71 +51,67 @@ const GREY = '#a09f9d';
 function isActiveColor(condition) { return condition ? PINK : GREY; };
 
 function togglePhotoElements() {
-  Array.from(document.querySelectorAll('.media-group')).forEach(a => {
-    if (!a.querySelector('.video')) {
-      $(a.parentElement).toggle(config.showPhotos);
-    }
-  });
-  $('#togglePhotos').css('backgroundColor', isActiveColor(config.showPhotos));
-  $('#togglePhotos').text(!config.showPhotos ? 'show photos' : 'hide photos');
+    document.querySelectorAll('.media-group > div:last-child:not(.video)').forEach(a => {
+        $(a.parentElement).toggle(config.showPhotos);
+    });
+    $('#togglePhotos').css('backgroundColor', isActiveColor(config.showPhotos));
+    $('#togglePhotos').text(!config.showPhotos ? 'show photos' : 'hide photos');
 }
 
 function hidePhotoOnlyAlbums() {
-  document.querySelectorAll('#albums').forEach(a => {
-    Array.from(a.children).forEach(a => {
-      if (!a.querySelector('.album-videos')) {
-        $(a).toggle(config.showPhotoAlbums);
-      }
+    document.querySelectorAll('div[id^=album]').forEach(a => {
+        if (!a.querySelector('.album-videos')) {
+            $(a).toggle(config.showPhotoAlbums);
+        }
     });
-  });
-  $('#togglePhotoAlbums').css('color', isActiveColor(!config.showPhotoAlbums));
+    $('#togglePhotoAlbums').css('color', isActiveColor(!config.showPhotoAlbums));
 }
 
 function infiniteScrollAndLazyLoading() {
-  // taken from index.html of erome mobile version
-  if (!document.querySelector('.pagination')) return;
-  const url = new URL(window.location.href);
-  let next_page = parseInt(url.searchParams.get('page')) || 2;
-  const limit = parseInt($('.pagination li:last-child()').prev().text()) || 50;
+    // taken from index.html of erome mobile version
+    if (!document.querySelector('.pagination')) return;
+    const url = new URL(window.location.href);
+    let next_page = parseInt(url.searchParams.get('page')) || 2;
+    const limit = parseInt($('.pagination li:last-child()').prev().text()) || 50;
 
-  const infinite = $('#page').infiniteScroll({
-    path: () => {
-      url.searchParams.set('page', next_page);
-      console.log(url.href);
-      return url.href;
-    },
-    append: '.page-content',
-    scrollThreshold: 800
-  });
+    const infinite = $('#page').infiniteScroll({
+        path: () => {
+            url.searchParams.set('page', next_page);
+            console.log(url.href);
+            return url.href;
+        },
+        append: '.page-content',
+        scrollThreshold: 800
+    });
 
-  $('#page').on('append.infiniteScroll', (event, body, path, items, response) => {
-    hidePhotoOnlyAlbums();
-    new LazyLoad();
-    scrollFix();
-    next_page++;
-    if (next_page > limit) {
-      infinite.destroy();
-    }
-  });
+    $('#page').on('append.infiniteScroll', (event, body, path, items, response) => {
+        hidePhotoOnlyAlbums();
+        new LazyLoad();
+        scrollFix();
+        next_page++;
+        if (next_page > limit) {
+            infinite.destroy();
+        }
+    });
 }
 
 function scrollFix() {
-  setTimeout(() => window.dispatchEvent(new Event('scroll')), 1000);
+    setTimeout(() => window.dispatchEvent(new Event('scroll')), 1000);
 }
 
 /******************************************* STATE ***********************************************/
 
 const config = {
-  showPhotos: false,
-  showPhotoAlbums: false
-};;
+    showPhotos: false,
+    showPhotoAlbums: false
+}
 
 function sync() {
-  Object.assign(config, { ...JSON.parse(localStorage.getItem("config")) });
+    Object.assign(config, { ...JSON.parse(localStorage.getItem("config")) });
 }
 
 function save() {
-  localStorage.setItem("config", JSON.stringify(config));
+    localStorage.setItem("config", JSON.stringify(config));
 }
 
 //=================================================================================================
@@ -123,29 +119,29 @@ function save() {
 const IS_ALBUM_PAGE = /^\/a\//.test(window.location.pathname);
 
 function pageAction() {
-  sync();
-  if (IS_ALBUM_PAGE) {
-    togglePhotoElements();
-  } else {
-    hidePhotoOnlyAlbums();
-  }
+    sync();
+    if (IS_ALBUM_PAGE) {
+        togglePhotoElements();
+    } else {
+        hidePhotoOnlyAlbums();
+    }
 }
 
 if (IS_ALBUM_PAGE) {
-  $('#user_name').parent().append('<button id="togglePhotos" class="btn btn-pink">show/hide photos</button>');
-  $('#togglePhotos').on('click', () => {
-    config.showPhotos = !config.showPhotos;
-    togglePhotoElements();
-    save();
-  });
+    $('#user_name').parent().append('<button id="togglePhotos" class="btn btn-pink">show/hide photos</button>');
+    $('#togglePhotos').on('click', () => {
+        config.showPhotos = !config.showPhotos;
+        togglePhotoElements();
+        save();
+    });
 } else {
-  infiniteScrollAndLazyLoading();
-  $('.navbar-nav').append('<li><a href="#" id="togglePhotoAlbums">video only</span></a></li>');
-  $('#togglePhotoAlbums').on('click', () => {
-    config.showPhotoAlbums = !config.showPhotoAlbums;
-    hidePhotoOnlyAlbums();
-    save();
-  });
+    infiniteScrollAndLazyLoading();
+    $('.navbar-nav').append('<li><a href="#" id="togglePhotoAlbums">video only</span></a></li>');
+    $('#togglePhotoAlbums').on('click', () => {
+        config.showPhotoAlbums = !config.showPhotoAlbums;
+        hidePhotoOnlyAlbums();
+        save();
+    });
 }
 
 window.addEventListener('focus', pageAction);
