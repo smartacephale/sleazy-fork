ea18b85d08313e6793bfbcf2627fb705f60f7362
smartacephale <atm.mormon@protonmail.com>
2024-07-03 23:07:19 +0300

    Update nhentai-improved.user.js

--- nhentai-improved.user.js
+++ nhentai-improved.user.js
@@ -2,12 +2,11 @@
 // @name         NHentai Improved
 // @namespace    http://tampermonkey.net/
 // @license      MIT
-// @version      1.2.1
+// @version      1.4
 // @description  Infinite scroll (optional). Filter by include/exclude phrases and languages. Search similar button
 // @author       smartacephale
 // @supportURL   https://github.com/smartacephale/sleazy-fork
 // @match        https://*.nhentai.net/*
-// @match        https://*.nhentai.to/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nhentai.net
 // @grant        GM_addStyle
 // @require      https://unpkg.com/vue@3.4.21/dist/vue.global.prod.js
@@ -72,6 +71,7 @@ class NHENTAI_RULES {
         const { pathname } = window.location;
 
         this.IS_VIDEO_PAGE = /^\/g\/\d+/.test(pathname);
+        this.IS_SEARCH_PAGE = /^\/search\//.test(pathname);
 
         this.PAGINATION = document.querySelector('.pagination');
         this.PAGINATION_LAST = parseInt(document.querySelector('.pagination .last')?.href.match(/\d+/)[0]) || 1;
@@ -121,30 +121,80 @@ const RULES = new NHENTAI_RULES();
 
 //====================================================================================================
 
-const EN = 2049;
-const CN = 2061;
-const JP = 1225;
-
-const langState = { filter: undefined }
-
-function filterByLang(lang) {
-    if (!lang && !langState.filter) return;
-    if (lang) langState.filter = lang;
-    if (!langState.filter) langState.filter = lang;
-    console.log('langState.filter ', langState.filter);
-    document.querySelectorAll('.cover .caption').forEach(e => {
-        const styles = window.getComputedStyle(e,':before');
-        e.closest('.gallery').style.display = styles.background.length !== langState.filter ? 'none' : 'inline-block';
-    });
+const DEFAULT_NHENTAI_STATE = {
+    english: true,
+    japanese: false,
+    chinese: false,
+    gayFilter: false,
+    fullColor: false
 }
 
-function filterByLanguageUI() {
-    const btnContainer = Array.from(document.querySelectorAll('.sort-type, .menu')).pop();
-    const btn = parseDOM(`<div class="sort-type"><a id="jp_l" href="#">🇯🇵</a> <a id="cn_l" href="#">🇨🇳</a> <a id="en_l" href="#">🇬🇧</a></div>`);
-    btnContainer.after(btn);
-    btn.querySelector('#jp_l').addEventListener('click', () => filterByLang(JP));
-    btn.querySelector('#cn_l').addEventListener('click', () => filterByLang(CN));
-    btn.querySelector('#en_l').addEventListener('click', () => filterByLang(EN));
+const LANGUAGES = { english: '🇬🇧', japanese: '🇯🇵', chinese: '🇨🇳' };
+
+function checkParamInQuery(param, state, url_) {
+    let url = url_ || window.location.search;
+    if (state && !url.includes(param)) {
+        url += `+${param}`;
+    }
+    if (!state && url.includes(param)) {
+        url = url.replace(`+${param}`, () => '');
+    }
+    return url;
+}
+
+function filterByLanguageUI(state) {
+    const btnContainer = Array.from(document.querySelectorAll('.sort-type')).pop();
+    const btns = parseDOM(`<div class="sort-type"></div>`);
+    Object.keys(LANGUAGES).forEach(lang => {
+        const btn = parseDOM(`<a href="#" ${state.custom[lang] ? 'style="background: rgba(59, 49, 70, 1)"' : ''}>${LANGUAGES[lang]}</a>`);
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            state.custom[lang] = !state.custom[lang];
+            window.location.href = checkParamInQuery(lang, state.custom[lang]);
+        });
+        btns.append(btn);
+    });
+    btnContainer.after(btns);
+}
+
+function filterByFullColor(state) {
+    const btnContainer = Array.from(document.querySelectorAll('.sort-type')).pop();
+    const btns = parseDOM(`<div class="sort-type"></div>`);
+    const btn = parseDOM(`<a href="#" ${state.custom.fullColor ? 'style="background: rgba(59, 49, 70, 1)"' : ''}>Full Color</a>`);
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        state.custom.fullColor = !state.custom.fullColor;
+        window.location.href = checkParamInQuery('full+color', state.custom.fullColor);
+    });
+    btns.append(btn);
+    btnContainer.after(btns);
+}
+
+
+function findSimilar(state) {
+    let tags = Array.from(document.querySelectorAll('.tags .tag[href^="/tag/"] .name')).map(tag => tag.innerText).join(" ").split(" ");
+    tags = Array.from(new Set(tags)).sort((a,b) => a.length < b.length);
+
+    const urls = {
+        searchSimilarVeryQuery: `/search/?q=${tags.join("+")}`,
+        searchSimilarQuery: `/search/?q=${tags.slice(0,10).join("+")}`,
+        searchSimilarLessQuery: `/search/?q=${tags.slice(0,5).join("+")}`,
+        searchSimilarKindofQuery: `/search/?q=${tags.reverse().slice(0,5).join("+")}`
+    }
+
+    Object.keys(urls).forEach(url => {
+        let res_url = urls[url];
+        Object.keys(LANGUAGES).forEach(lang => {
+            res_url = checkParamInQuery(lang, state.custom[lang], res_url);
+        });
+        urls[url] = res_url;
+    });
+
+    document.querySelector('.buttons').append(
+        parseDOM(`<a href="${urls.searchSimilarVeryQuery}" class="btn" style="background: rgba(59, 49, 70, 1)"><i class="fa fa-search"></i> Very Similar</a>`),
+        parseDOM(`<a href="${urls.searchSimilarQuery}" class="btn" style="background: rgba(59, 49, 70, .9)"><i class="fa fa-search"></i> Similar</a>`),
+        parseDOM(`<a href="${urls.searchSimilarLessQuery}" class="btn" style="background: rgba(59, 49, 70, .8)"><i class="fa fa-search"></i> Less Similar</a>`),
+        parseDOM(`<a href="${urls.searchSimilarKindofQuery}" class="btn" style="background: rgba(59, 49, 70, .7)"><i class="fa fa-search"></i> Kindof Similar</a>`));
 }
 
 //====================================================================================================
@@ -153,39 +203,26 @@ console.log(LOGO);
 
 const SCROLL_RESET_DELAY = 350;
 
-const defaultState = new DefaultState({ DURATION_FILTER: false });
+const defaultState = new DefaultState({ DURATION_FILTER: false }, DEFAULT_NHENTAI_STATE);
 const { state, stateLocale } = defaultState;
 const { filter_, handleLoadedHTML } = new DataManager(RULES, state);
 defaultState.setWatchers(filter_);
 
-const proxyHTMLHandle = new Proxy(handleLoadedHTML, {
-    apply(target, ctx, args) {
-        setTimeout(filterByLang, 100);
-        return Reflect.apply(...arguments)
-    }
-});
-
 if (RULES.IS_VIDEO_PAGE) {
-    let tags = Array.from(document.querySelectorAll('.tags .tag[href^="/tag/"] .name')).map(tag => tag.innerText).join(" ").split(" ");
-    tags = Array.from(new Set(tags)).sort((a,b) => a.length < b.length);
-    const searchSimilarVeryQuery = `/search/?q=${tags.join("+")}`;
-    const searchSimilarQuery = `/search/?q=${tags.slice(0,10).join("+")}`;
-    const searchSimilarLessQuery = `/search/?q=${tags.slice(0,5).join("+")}`;
-    const searchSimilarKindofQuery = `/search/?q=${tags.reverse().slice(0,5).join("+")}`;
-    document.querySelector('.buttons').append(
-        parseDOM(`<a href="${searchSimilarVeryQuery}" class="btn" style="background: #3b3146"><i class="fa fa-search"></i> Very Similar</a>`),
-        parseDOM(`<a href="${searchSimilarQuery}" class="btn" style="background: #3b3146"><i class="fa fa-search"></i> Similar</a>`),
-        parseDOM(`<a href="${searchSimilarLessQuery}" class="btn" style="background: #3b3146"><i class="fa fa-search"></i> Less Similar</a>`),
-        parseDOM(`<a href="${searchSimilarKindofQuery}" class="btn" style="background: #3b3146"><i class="fa fa-search"></i> Kindof Similar</a>`));
+    findSimilar(state);
 }
 
 if (RULES.CONTAINER) {
-    proxyHTMLHandle(RULES.CONTAINER);
+    handleLoadedHTML(RULES.CONTAINER);
+}
+
+if (RULES.IS_SEARCH_PAGE) {
+    filterByLanguageUI(state);
+    filterByFullColor(state);
 }
 
 if (RULES.PAGINATION) {
-    filterByLanguageUI();
-    const paginationManager = new PaginationManager(state, stateLocale, RULES, proxyHTMLHandle, SCROLL_RESET_DELAY);
+    const paginationManager = new PaginationManager(state, stateLocale, RULES, handleLoadedHTML, SCROLL_RESET_DELAY);
 }
 
 const ui = new VueUI(state, stateLocale);
