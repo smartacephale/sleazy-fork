323cb15647ac0595aa4de3ebe3415daa30379400
smartacephale <atm.mormon@protonmail.com>
2024-07-15 11:47:08 +0300

    Update data-manager.user.js

--- utils/data-manager.user.js
+++ utils/data-manager.user.js
@@ -83,14 +83,6 @@ class DataManager {
             }
         }
     }
-
-    /**
-    * @todo redo filters
-    * @body
-        - word, word*, word1|word2*|word3*
-        - not hide but delete by -word*
-        - filter uploaders: -a:word
-    */
     
     setupFilters() {
         Object.keys(this.dataFilters).forEach(k => {
