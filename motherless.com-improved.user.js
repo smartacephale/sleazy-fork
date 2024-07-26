573b956c2ce1ad313db9a92002e9f969846eef8c
smartacephale <atm.mormon@protonmail.com>
2024-07-26 18:59:45 +0300

    Update motherless.com-improved.user.js

--- motherless.com-improved.user.js
+++ motherless.com-improved.user.js
@@ -4,7 +4,7 @@
 // @author       smartacephale
 // @supportURL   https://github.com/smartacephale/sleazy-fork
 // @license      MIT
-// @version      2.6.9
+// @version      2.6.91
 // @description  Infinite scroll (optional). Filter by duration and key phrases. Download button fixed. Reveal all related galleries to video at desktop. Galleries and tags url rewritten and redirected to video/image section if available
 // @match        https://motherless.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=motherless.com
@@ -156,7 +156,8 @@ function animate() {
 function fixURLs() {
     document.querySelector('a[href^="https://motherless.com/random/image"]').href = "https://motherless.com/m/calypso_jim_asi";
     document.querySelectorAll(('.gallery-container')).forEach(g => {
-        const hasVideos = parseInt(g.innerText.match(/(\d+) videos/gi)?.[0]) > 0;
+        const hasVideos = parseInt(g.innerText.match(/([\d|\.]+)k? videos/gi)?.[0]) > 0;
+        console.log({hasVideos, text: g.innerText});
         const header = hasVideos ? '/GV' : '/GI';
         g.querySelectorAll('a').forEach(a => { a.href = a.href.replace(/\/G/, () => header); });
     });
