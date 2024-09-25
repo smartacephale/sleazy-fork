69563f6737a914c0f81e1fe89f6119ea00db19d6
smartacephale <atm.mormon@protonmail.com>
2024-09-25 03:39:24 +0000

    Update motherless.com-improved.user.js

--- motherless.com-improved.user.js
+++ motherless.com-improved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Motherless.com Improved
 // @namespace    http://tampermonkey.net/
-// @version      2.911
+// @version      2.912
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration and key phrases. Download button fixed. Reveal all related galleries to video at desktop. Galleries and tags url rewritten and redirected to video/image section if available
 // @author       smartacephale
@@ -160,7 +160,7 @@ function fixURLs() {
     document.querySelectorAll('a[href^="/term/"]').forEach(a => {
         a.href = a.href.replace(/[\w|+]+$/, (v) => `videos/${v}?term=${v}&range=0&size=0&sort=date`);
     });
-    document.querySelectorAll('a[href^="/g/"]').forEach(a => a.href = a.href.replace(/\/g\//, '/gv/'));
+    document.querySelectorAll('#media-groups-container a[href^="/g/"]').forEach(a => a.href = a.href.replace(/\/g\//, '/gv/'));
 }
 
 //====================================================================================================
