5ca1be81c6bfb38cdc24e9f5b55cbf233640f15b
smartacephale <atm.mormon@protonmail.com>
2025-05-21 23:28:14 +0000

    Update spankbang.com-improved.user.js

--- spankbang.com-improved.user.js
+++ spankbang.com-improved.user.js
@@ -1,24 +1,22 @@
 // ==UserScript==
 // @name         SpankBang.com Improved
 // @namespace    http://tampermonkey.net/
-// @version      2.0.1
+// @version      2.1.0
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration, include/exclude phrases
 // @author       smartacephale
 // @supportURL   https://github.com/smartacephale/sleazy-fork
 // @match        https://*.spankbang.com/*
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.1.9/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.3.1/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.4.9/dist/jabroni-outfit.umd.js
-// @require      https://update.greasyfork.org/scripts/494204/data-manager.user.js?version=1458190
-// @require      https://update.greasyfork.org/scripts/494205/pagination-manager.user.js?version=1459738
 // @run-at       document-idle
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=spankbang.com
 // @downloadURL https://update.sleazyfork.org/scripts/493946/SpankBangcom%20Improved.user.js
 // @updateURL https://update.sleazyfork.org/scripts/493946/SpankBangcom%20Improved.meta.js
 // ==/UserScript==
-/* globals $ DataManager PaginationManager */
 
+const { getAllUniqueParents, timeToSeconds, parseDom, sanitizeStr, DataManager, InfiniteScroller } = window.bhutils;
 Object.assign(unsafeWindow, { bhutils: window.bhutils });
 const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI, DefaultScheme } = window.jabronioutfit;
 
@@ -48,11 +46,16 @@ const LOGO = `
 ⠀⠀⠈⡀⠂⠐⠀⢑⠜⡌⢎⢪⠪⡪⡪⡪⢪⠪⡪⢪⠪⡪⡪⢪⠪⡊⡎⡜⡌⡎⢎⢎⢎⢎⠎⡎⡪⠢⡑⢌⢪⢘⢔⠱⡡⢣⢃⢇⠕⡕⢅⢇⢣⢱⢑⢕⠸⡐⡱⡘`;
 
 class SPANKBANG_RULES {
+    delay = 300;
+
     constructor() {
-        this.PAGINATION = document.querySelector('.paginate-bar, .pagination');
-        this.PAGINATION_LAST = parseInt(
+        this.IS_VIDEO_PAGE = /^\/\w+\/video\//.test(location.pathname);
+        this.IS_SEARCH_PAGE = location.pathname.startsWith('/s/');
+        this.paginationElement = document.querySelector('.paginate-bar, .pagination');
+        this.paginationLast = parseInt(
             document.querySelector('.paginate-bar .status span')?.innerText.match(/\d+/)?.[0] ||
             Array.from(document.querySelectorAll('.pagination a'))?.at(-2)?.innerText);
+        Object.assign(this, this.URL_DATA());
         this.CONTAINER = document.querySelectorAll('.results .video-list')[0];
         this.HAS_VIDEOS = !!this.GET_THUMBS(document.body).length > 0;
     }
@@ -73,22 +76,22 @@ class SPANKBANG_RULES {
     }
 
     THUMB_DATA(thumb) {
-        const title = bhutils.sanitizeStr(thumb.querySelector('[title]')?.getAttribute('title'));
-        const duration = bhutils.timeToSeconds(thumb.querySelector('span.l')?.innerText);
+        const title = sanitizeStr(thumb.querySelector('[title]')?.getAttribute('title'));
+        const duration = timeToSeconds(thumb.querySelector('span.l')?.innerText);
         return { title, duration };
     }
 
     URL_DATA() {
         const url = new URL(window.location.href);
-        const offset = parseInt(url.pathname.match(/\/(\d+)\/?$/)?.pop()) || 1;
-        if (!/\/\d+\/$/.test(url.pathname)) url.pathname = `${url.pathname}/${offset}/`;
+        const paginationOffset = parseInt(url.pathname.match(/\/(\d+)\/?$/)?.pop()) || 1;
+        if (!/\/\d+\/$/.test(url.pathname)) url.pathname = `${url.pathname}/${paginationOffset}/`;
 
-        const iteratable_url = n => {
+        const paginationUrlGenerator = n => {
             url.pathname = url.pathname.replace(/\/\d+\/$/, `/${n}/`);
             return url.href;
         };
 
-        return { offset, iteratable_url };
+        return { paginationOffset, paginationUrlGenerator };
     }
 }
 
@@ -97,8 +100,9 @@ const RULES = new SPANKBANG_RULES();
 //====================================================================================================
 
 function createPreviewElement(src, mount) {
-    const elem = bhutils.parseDom(`
-      <div class="video-js vjs-controls-disabled vjs-touch-enabled vjs-workinghover vjs-v7 vjs-playing vjs-has-started mp4t_video-dimensions vjs-user-inactive" id="mp4t_video" tabindex="-1" lang="en" translate="no" role="region" aria-label="Video Player" style="opacity: 1;">
+    const elem = parseDom(`
+      <div class="video-js vjs-controls-disabled vjs-touch-enabled vjs-workinghover vjs-v7 vjs-playing vjs-has-started
+        mp4t_video-dimensions vjs-user-inactive" id="mp4t_video" tabindex="-1" lang="en" translate="no" role="region" aria-label="Video Player" style="opacity: 1;">
         <video id="mp4t_video_html5_api" class="vjs-tech" tabindex="-1" muted="muted" playsinline="playsinline" autoplay="autoplay" src="${src}"></video>
         <div class="vjs-poster vjs-hidden" tabindex="-1" aria-disabled="false"></div>
         <div class="vjs-text-track-display" translate="yes" aria-live="off" aria-atomic="true">
@@ -147,29 +151,49 @@ function unmute(tries = 10) {
 
 //====================================================================================================
 
-console.log(LOGO);
+function createInfiniteScroller() {
+  const iscroller = new InfiniteScroller({
+    enabled: state.infiniteScrollEnabled,
+    handleHtmlCallback: handleLoadedHTML,
+    ...RULES,
+  }).onScroll(({paginationLast, paginationOffset}) => {
+    stateLocale.pagIndexLast = paginationLast;
+    stateLocale.pagIndexCur = paginationOffset;
+  }, true);
 
-const SCROLL_RESET_DELAY = 350;
+  store.subscribe(() => {
+    iscroller.enabled = state.infiniteScrollEnabled;
+  });
+}
+
+//====================================================================================================
+
+function route() {
+    if (RULES.IS_SEARCH_PAGE) {
+        [...document.querySelectorAll('.main_results .main_content_container')].slice(1).forEach(c => {
+            if (c.querySelector('.video-list, a.go, h2.main_content_title, .channels_scroll_list')) c.remove()
+        });
+    }
+
+    if (RULES.HAS_VIDEOS) {
+        animate();
+        new JabroniOutfitUI(store);
+        getAllUniqueParents(RULES.GET_THUMBS(document.body)).forEach(c => handleLoadedHTML(c, c));
+    }
+
+    if (RULES.paginationElement) {
+        createInfiniteScroller();
+    }
+
+    if (RULES.IS_VIDEO_PAGE) unmute();
+}
+
+//====================================================================================================
+
+console.log(LOGO);
 
 const store = new JabroniOutfitStore(defaultStateWithDuration);
 const { state, stateLocale } = store;
 const { applyFilters, handleLoadedHTML } = new DataManager(RULES, state);
 store.subscribe(applyFilters);
-
-if (location.pathname.startsWith('/s/')) {
-  Array.from(document.querySelectorAll('.main_results .main_content_container')).slice(1).forEach(c => {
-    if (c.querySelector('.video-list, a.go, h2.main_content_title, .channels_scroll_list')) c.remove()
-  });
-}
-
-if (RULES.HAS_VIDEOS) {
-    animate();
-    new JabroniOutfitUI(store);
-    bhutils.getAllUniqueParents(RULES.GET_THUMBS(document.body)).forEach(c => handleLoadedHTML(c, c));
-}
-
-if (RULES.PAGINATION) {
-    new PaginationManager(state, stateLocale, RULES, handleLoadedHTML, SCROLL_RESET_DELAY);
-}
-
-unmute();
+route();
