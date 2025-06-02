7e93affe4a52c29d3ba4d43b95da912be43d0454
smartacephale <atm.mormon@protonmail.com>
2025-06-02 06:34:31 +0000

    Update javhdporn.net-improved.user.js

--- javhdporn.net-improved.user.js
+++ javhdporn.net-improved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         javhdporn.net Improved
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration, include/exclude phrases
 // @author       smartacephale
@@ -9,13 +9,12 @@
 // @match        https://*.javhdporn.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=javhdporn.net
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.3.1/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.3.6/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.4.9/dist/jabroni-outfit.umd.js
 // @run-at       document-idle
 // ==/UserScript==
 
-const { timeToSeconds, sanitizeStr, DataManager, InfiniteScroller } = window.bhutils;
-Object.assign(unsafeWindow, { bhutils: window.bhutils });
+const { timeToSeconds, sanitizeStr, DataManager, createInfiniteScroller } = window.bhutils;
 const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI, DefaultScheme } = window.jabronioutfit;
 
 const LOGO = `
@@ -62,68 +61,62 @@ const LOGO = `
 ⡠⠡⡐⠄⡁⠆⠕⢌⠔⠔⠜⡞⠷⢽⠽⡜⡯⡇⡿⢝⠇⡨⢐⠠⠨⠠⢐⠠⠂⡂⠌⠄⠅⡂⠌⡐⠄⠅⠄⠪⠆⠯⠺⠝⠝⠣⠃⡓⠚⢙⠊⢃⠁⡂⠅⡂⢢⢑⢕⠁`;
 
 class JAVHDPORN_RULES {
-    delay = 350;
+  delay = 350;
 
-    constructor() {;
-        this.paginationElement = document.querySelector('.pagination');
-        this.paginationLast = parseInt(Array.from(this.paginationElement?.querySelectorAll('a') || [], (a) => a.href.match(/\d+/g)?.pop())?.pop() || 1);
-        Object.assign(this, this.URL_DATA());
-        this.CONTAINER = this.GET_THUMBS(document.body)?.[0].parentElement;
-    }
+  paginationElement = document.querySelector('.pagination');
+  paginationLast = parseInt(
+    Array.from(document.querySelectorAll('.pagination a') || [], (a) =>
+      a.href.match(/\d+/g)?.pop(),
+    )?.pop() || 1,
+  );
+  CONTAINER = this.GET_THUMBS(document.body)?.[0].parentElement;
 
-    THUMB_URL(thumb) { return thumb.querySelector('a').href; }
+  constructor() {
+    Object.assign(this, this.URL_DATA());
+  }
 
-    GET_THUMBS(html) { return Array.from(html.querySelectorAll('article.thumb-block')); }
+  THUMB_URL(thumb) {
+    return thumb.querySelector('a').href;
+  }
 
-    THUMB_IMG_DATA(thumb) { return ({}); }
+  GET_THUMBS(html) {
+    return Array.from(html.querySelectorAll('article.thumb-block'));
+  }
 
-    THUMB_DATA(thumb) {
-        const title = sanitizeStr(thumb.querySelector('header.entry-header')?.innerText);
-        const duration = timeToSeconds(thumb.querySelector('.duration')?.innerText);
-        return { title, duration };
-    }
+  THUMB_IMG_DATA(thumb) {
+    return {};
+  }
 
-    URL_DATA() {
-        const url = new URL(window.location.href);
-        const paginationOffset = parseInt(location.pathname.match(/\/page\/(\d+)/)?.pop() || 1);
-        if (paginationOffset < 2) url.pathname = `${url.pathname}/page/${paginationOffset}/`;
+  THUMB_DATA(thumb) {
+    const title = sanitizeStr(thumb.querySelector('header.entry-header')?.innerText);
+    const duration = timeToSeconds(thumb.querySelector('.duration')?.innerText);
+    return { title, duration };
+  }
 
-        const paginationUrlGenerator = n => {
-            return url.href.replace(/\/page\/\d+/, () => `/page/${n}`);
-        }
+  URL_DATA() {
+    const url = new URL(location.href);
+    const paginationOffset = parseInt(location.pathname.match(/\/page\/(\d+)/)?.pop() || 1);
+    if (paginationOffset < 2) url.pathname = `${url.pathname}/page/${paginationOffset}/`;
 
-        return { paginationOffset, paginationUrlGenerator }
-    }
+    const paginationUrlGenerator = (n) => {
+      return url.href.replace(/\/page\/\d+/, () => `/page/${n}`);
+    };
+
+    return { paginationOffset, paginationUrlGenerator };
+  }
 }
 
 const RULES = new JAVHDPORN_RULES();
 
 //====================================================================================================
 
-function createInfiniteScroller() {
-  const iscroller = new InfiniteScroller({
-    enabled: state.infiniteScrollEnabled,
-    handleHtmlCallback: handleLoadedHTML,
-    ...RULES,
-  }).onScroll(({paginationLast, paginationOffset}) => {
-    stateLocale.pagIndexLast = paginationLast;
-    stateLocale.pagIndexCur = paginationOffset;
-  }, true);
-
-  store.subscribe(() => {
-    iscroller.enabled = state.infiniteScrollEnabled;
-  });
-}
-
-//====================================================================================================
-
 function router() {
   if (RULES.CONTAINER) {
-      handleLoadedHTML(RULES.CONTAINER);
+    handleLoadedHTML(RULES.CONTAINER);
   }
 
   if (RULES.paginationElement) {
-    createInfiniteScroller();
+    createInfiniteScroller(store, handleLoadedHTML, RULES);
   }
 
   new JabroniOutfitUI(store);
@@ -137,4 +130,5 @@ const store = new JabroniOutfitStore(defaultStateWithDuration);
 const { state, stateLocale } = store;
 const { applyFilters, handleLoadedHTML } = new DataManager(RULES, state);
 store.subscribe(applyFilters);
+
 router();
