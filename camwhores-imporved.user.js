db24ab81fe9735846ba2695d80b91b977f8d5a1b
smartacephale <atm.mormon@protonmail.com>
2025-09-23 06:24:35 +0000

    optional friend request

--- camwhores-imporved.user.js
+++ camwhores-imporved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CamWhores.tv Improved
 // @namespace    http://tampermonkey.net/
-// @version      2.4.0
+// @version      2.4.3
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration, private/public, include/exclude phrases. Mass friend request button. Download button
 // @author       smartacephale
@@ -11,7 +11,7 @@
 // @exclude      https://*.camwhores.tv/*mode=async*
 // @grant        GM_addStyle
 // @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.2/dist/billy-herrington-utils.umd.js
-// @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.4/dist/jabroni-outfit.umd.js
+// @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.5/dist/jabroni-outfit.umd.js
 // @require      https://cdn.jsdelivr.net/npm/lskdb@1.0.2/dist/lskdb.umd.js
 // @run-at       document-idle
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=camwhores.tv
@@ -280,6 +280,8 @@ async function requestAccess() {
   setTimeout(processFriendship, FRIEND_REQUEST_INTERVAL);
 }
 
+Object.assign(window, { requestAccess });
+
 async function checkPrivateVidsAccess() {
     const checkAccess = async (item) => {
         const videoURL = item.firstElementChild.href;
@@ -290,8 +292,10 @@ async function checkPrivateVidsAccess() {
         const haveAccess = !doc.querySelector('.no-player');
 
         if (!haveAccess) {
-          const uid = doc.querySelector('.message a').href.match(/\d+/).at(-1);
-          lskdb.setKey(uid);
+          if (store.state.autoRequestAccess) {
+            const uid = doc.querySelector('.message a').href.match(/\d+/).at(-1);
+            lskdb.setKey(uid);
+          }
           item.classList.add('haveNoAccess');
         } else {
           item.classList.add('haveAccess');
@@ -345,8 +349,8 @@ function clearMessages() {
     }
     spool.run();
 
-    let c = 0;
     function checkMessageHistory(url) {
+        let c = 0;
         fetchHtml(url).then(html => {
             const hasFriendRequest = html.querySelector('input[value=confirm_add_to_friends]');
             const hasOriginalText = html.querySelector('.original-text')?.innerText;
@@ -383,10 +387,10 @@ function route() {
     if (RULES.IS_MEMBER_PAGE) {
       createFriendButton();
     }
-    if (RULES.HAS_VIDEOS) {
-      defaultSchemeWithPrivacyFilter.privacyFilter.push(
-        { type: 'button', innerText: 'check access 🔓', callback: requestAccess });
-    }
+  }
+
+  if (!RULES.HAS_VIDEOS || !RULES.IS_LOGGED_IN) {
+    delete defaultSchemeWithPrivacyFilter.privacyFilter;
   }
 
   shoudIScroll();
