85b720e0db0dcfa4ad7bf484a08bc7d16a8f9735
smartacephale <atm.mormon@protonmail.com>
2025-10-27 12:48:35 +0000

    Bump version to 4.0.0 and update dependencies
    Updated version number and dependencies for improved functionality.

--- motherless.com-improved.user.js
+++ motherless.com-improved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Motherless.com Improved
 // @namespace    http://tampermonkey.net/
-// @version      3.3.1
+// @version      4.0.0
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration and key phrases. Download button fixed. Reveal all related galleries to video at desktop. Galleries and tags url rewritten and redirected to video/image section if available.
 // @author       smartacephale
@@ -10,7 +10,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=motherless.com
 // @grant        unsafeWindow
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.2/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.5.0/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.4/dist/jabroni-outfit.umd.js
 // @run-at       document-idle
 // @downloadURL https://update.sleazyfork.org/scripts/492238/Motherlesscom%20Improved.user.js
@@ -18,7 +18,16 @@
 // ==/UserScript==
 /* globals $ */
 
-const { Tick, fetchWith, timeToSeconds, replaceElementTag, isMob, sanitizeStr, getAllUniqueParents, DataManager, createInfiniteScroller } = window.bhutils;
+const {
+  fetchWith,
+  timeToSeconds,
+  replaceElementTag,
+  sanitizeStr,
+  getAllUniqueParents,
+  DataManager,
+  createInfiniteScroller,
+  getPaginationStrategy
+} = window.bhutils;
 const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI } = window.jabronioutfit;
 
 const LOGO = `
@@ -45,47 +54,32 @@ const LOGO = `
 ⣯⣟⡾⣽⣳⢯⡿⣽⢯⣟⣷⢻⣞⣭⢿⣹⠶⣏⡿⣽⢯⣟⡿⣿⣿⣿⣿⣿⣿⣿⢷⣯⣛⢾⡹⣎⠷⣎⢷⡺⣜⢧⣛⣥⢻⡵⣣⢟⡼⣋⠦⡙⠰⢂⠎⠢⠔⡡⢊⠄
 ⡿⣽⣻⢷⣯⠿⣽⣳⣟⡾⣞⠿⣼⢎⡷⣭⢻⡞⣽⣳⣟⡾⣿⣿⣿⣿⣽⡷⢾⣽⣻⢶⣫⣗⣻⣜⡻⣜⢧⡻⣜⢧⡻⣜⡳⣞⡵⣫⢞⣥⣶⣷⣿⣶⣿⣿⣿⣿⣿⣿`;
 
-// Enable internal downloader
 unsafeWindow.__is_premium = true;
 
 class MOTHERLESS_RULES {
-  delay = 150;
-
   IS_SEARCH = /^\/term\//.test(location.pathname);
-  CONTAINER = [...document.querySelectorAll('.content-inner')].pop();
+  container = [...document.querySelectorAll('.content-inner')].pop();
 
-  paginationElement = document.querySelector('.pagination_link, .ml-pagination');
-  paginationLast = parseInt(
-    document
-      .querySelector('.pagination_link a:last-child')
-      ?.previousSibling.innerText.replace(',', '') ||
-      document.querySelector('.ml-pagination li:last-child')?.innerText.replace(',', ''),
-  );
+  paginationStrategy = getPaginationStrategy({
+    paginationSelector: '.pagination_link, .ml-pagination'
+  });
 
-  paginationOffset = parseInt(new URLSearchParams(location.search).get('page')) || 1;
-
-  paginationUrlGenerator = (offset) => {
-    const url = new URL(location.href);
-    url.searchParams.set('page', offset);
-    return url.href;
-  };
-
-  GET_THUMBS(html) {
+  getThumbs(html) {
     return html.querySelectorAll('.thumb-container, .mobile-thumb');
   }
 
-  THUMB_URL(thumb) {
+  getThumbUrl(thumb) {
     return thumb.querySelector('a').href;
   }
 
-  THUMB_DATA(thumb) {
+  getThumbData(thumb) {
     const uploader = sanitizeStr(thumb.querySelector('.uploader')?.innerText);
     const title = sanitizeStr(thumb.querySelector('.title')?.innerText).concat(` user:${uploader}`);
     const duration = timeToSeconds(thumb.querySelector('.size')?.innerText);
     return { title, duration };
   }
 
-  THUMB_IMG_DATA(thumb) {
+  getThumbImgData(thumb) {
     const img = thumb.querySelector('.static');
     const imgSrc = img.getAttribute('src');
     return { img, imgSrc };
@@ -99,7 +93,7 @@ const RULES = new MOTHERLESS_RULES();
 function animate() {
   $(RULES.CONTAINER).find('a, div, span, ul, li, p, button').off();
   const ANIMATION_INTERVAL = 500;
-  const tick = new Tick(ANIMATION_INTERVAL);
+  const tick = new window.bhutils.Tick(ANIMATION_INTERVAL);
   let container;
 
   function handleLeave(e) {
@@ -120,7 +114,8 @@ function animate() {
       target.classList.contains('animating') ||
       target.parentElement.parentElement.classList.contains('image') ||
       target.getAttribute('src') === target.getAttribute('data-strip-src')
-    ) return;
+    )
+      return;
     target.classList.toggle('animating');
 
     container = target.parentElement.parentElement;
@@ -131,7 +126,7 @@ function animate() {
     const d = $(container.querySelector('.img-container'));
     const m = $(
       target.nextElementSibling ||
-        '<div style="z-index: 8; position: absolute; top: -11px;"></div>',
+      '<div style="z-index: 8; position: absolute; top: -11px;"></div>',
     );
     if (!target.nextElementSibling) {
       $(target.parentElement).append(m);
@@ -164,7 +159,7 @@ function fixURLs() {
   document.querySelector('a[href^="https://motherless.com/random/image"]').href =
     'https://motherless.com/m/calypso_jim_asi';
   document.querySelectorAll('.gallery-container').forEach((g) => {
-    const hasVideos = parseInt(g.innerText.match(/([\d|\.]+)k? videos/gi)?.[0]) > 0;
+    const hasVideos = parseInt(g.innerText.match(/([\d|.]+)k? videos/gi)?.[0]) > 0;
     const header = hasVideos ? '/GV' : '/GI';
     g.querySelectorAll('a').forEach((a) => {
       a.href = a.href.replace(/\/G/, () => header);
@@ -228,7 +223,7 @@ GM_addStyle(`
 function applySearchFilters() {
   let pathname = window.location.pathname;
   const wordsToFilter =
-    store.state.filterExcludeWords.replace(/f\:/g, '').match(/(?<!user:)\b\w+\b(?!\s*:)/g) || [];
+    store.state.filterExcludeWords.replace(/f:/g, '').match(/(?<!user:)\b\w+\b(?!\s*:)/g) || [];
   wordsToFilter
     .filter((w) => !pathname.includes(w))
     .forEach((w) => {
@@ -244,14 +239,14 @@ function applySearchFilters() {
 function route() {
   desktopAddMobGalleries().then(() => fixURLs());
 
-  if (RULES.paginationElement) {
+  if (RULES.paginationStrategy.hasPagination) {
     createInfiniteScroller(store, parseData, RULES);
     animate();
   }
 
-  if (RULES.GET_THUMBS(document.body).length > 0) {
+  if (RULES.getThumbs(document.body).length > 0) {
     new JabroniOutfitUI(store);
-    getAllUniqueParents(RULES.GET_THUMBS(document.body)).forEach((c) => {
+    getAllUniqueParents(RULES.getThumbs(document.body)).forEach((c) => {
       parseData(c, c, true);
     });
   }
