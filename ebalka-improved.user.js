caadbc9cba53bb27eb64dcc66a6fe7404eb7da3b
smartacephale <atm.mormon@protonmail.com>
2025-10-27 11:59:05 +0000

    Upgrade version and update dependencies in user script

--- ebalka-improved.user.js
+++ ebalka-improved.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         ebalka improved
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      2.0.0
 // @license      MIT
 // @description  Infinite scroll. Filter by duration, include/exclude phrases
 // @author       smartacephale
 // @supportURL   https://github.com/smartacephale/sleazy-fork
 // @match        https://*ebalka.*.*/*
+// @match        https://b.ebalka.zip/*
 // @match        https://*.ebalk*.*/*
 // @match        https://*.fuckingbear*.*/*
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.2/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.9/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.4/dist/jabroni-outfit.umd.js
 // @run-at       document-idle
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wwwa.ebalka.link
@@ -18,7 +19,7 @@
 // @updateURL https://update.sleazyfork.org/scripts/509735/ebalka%20improved.meta.js
 // ==/UserScript==
 
-const { timeToSeconds, sanitizeStr, parseDom, DataManager, createInfiniteScroller, parseDataParams } = window.bhutils;
+const { timeToSeconds, sanitizeStr, parseDom, DataManager, createInfiniteScroller, getPaginationStrategy } = window.bhutils;
 const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI } = window.jabronioutfit;
 
 const LOGO = `
@@ -60,68 +61,34 @@ const LOGO = `
 ⣿⣞⢗⣗⢯⣞⢽⡺⣝⡜⡜⡜⡜⡜⡜⡔⡕⢕⢱⣑⡧⡣⡊⢎⠔⢌⠢⡡⢱⠸⡸⣺⡽⣽⣳⢽⢽⣺⢝⣮⣳⢳⡫⣗⡿⡽⡪⡢⡑⢌⢂⠪⠨⡨⠪⡸⢸⢮⢯⢯`;
 
 class EBALKA_RULES {
-  delay = 250;
-
-  paginationElement = [...document.querySelectorAll('.pagination')].pop();
-  CONTAINER = [...document.querySelectorAll('.content__video')].pop();
+  container = [...document.querySelectorAll('.content__video')].pop();
   HAS_VIDEOS = !!document.querySelector('.card_video');
 
   constructor() {
-    const dataparams = parseDataParams(
-      document.querySelector('.pagination__item.last').getAttribute('data-parameters'),
-    );
-    const lastfrom =
-      dataparams[
-        Object.keys(dataparams)
-          .filter((k) => k.includes('from'))
-          ?.pop()
-      ];
-    this.paginationLast = parseInt(lastfrom) || 1;
-    Object.assign(this, this.URL_DATA());
+    this.paginationStrategy = getPaginationStrategy({
+      paginationSelector: '.pagination:not([id *= member])'
+    });
+
+    const pagination = this.paginationStrategy.getPaginationElement();
+    this.HAS_PAGINATION = pagination && pagination !== document;
   }
 
-  GET_THUMBS(html) {
+  getThumbs(html) {
     return html.querySelectorAll('.card_video');
   }
 
-  THUMB_IMG_DATA() { return ({}); }
+  getThumbImgData() { return ({}); }
 
-  THUMB_URL(thumb) {
+  getThumbUrl(thumb) {
     return thumb.querySelector('.root__link').href;
   }
 
-  THUMB_DATA(thumb) {
+  getThumbData(thumb) {
     const title = sanitizeStr(thumb.querySelector('.card__title').innerText);
     const duration = timeToSeconds(
       [...thumb.querySelector('.card__spot').children].pop().innerText,
     );
     return { title, duration };
-  }
-
-  URL_DATA() {
-    const url = new URL(window.location.href);
-    const paginationOffset =
-      parseInt(
-        document.querySelector('.pagination__item_active,input.pagination__item').innerText,
-      ) || 1;
-    const el = document.querySelector('.pagination__item.next');
-
-    const attrs = {
-      mode: 'async',
-      function: 'get_block',
-      block_id: el?.getAttribute('data-block-id'),
-      ...bhutils.parseDataParams(el?.getAttribute('data-parameters')),
-    };
-
-    Object.keys(attrs).forEach((k) => url.searchParams.set(k, attrs[k]));
-
-    const paginationUrlGenerator = (n) => {
-      Object.keys(attrs).forEach((k) => k.includes('from') && url.searchParams.set(k, n));
-      url.searchParams.set('_', Date.now());
-      return url.href;
-    };
-
-    return { paginationOffset, paginationUrlGenerator };
   }
 }
 
@@ -156,19 +123,19 @@ function animate() {
     thumb.addEventListener('mouseleave', removeElem, { once: true });
   }
 
-  RULES.CONTAINER.addEventListener('mouseover', handleThumbHover);
+  RULES.container.addEventListener('mouseover', handleThumbHover);
 }
 
 //====================================================================================================
 
 function route() {
-  if (RULES.paginationElement) {
+  if (RULES.HAS_PAGINATION) {
     createInfiniteScroller(store, parseData, RULES);
   }
 
   if (RULES.HAS_VIDEOS) {
     animate();
-    parseData(RULES.CONTAINER);
+    parseData(RULES.container);
     new JabroniOutfitUI(store);
   }
 }
