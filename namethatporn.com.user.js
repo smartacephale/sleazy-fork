ddc4f11ba38163ce4c80fa914ef418c9c3c0e28f
orangutan <atm.mormon@protonmail.com>
2025-10-27 23:24:33 +0000

    Upgrade userscript version and refactor methods
    Updated version and dependencies for the userscript, refactored methods for better readability, and improved pagination handling.

--- namethatporn.com.user.js
+++ namethatporn.com.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NamethatPorn Improved
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      2.0.0
 // @license      MIT
 // @description  Infinite scroll (optional). Filter by duration, include/exclude phrases
 // @author       smartacephale
@@ -9,13 +9,18 @@
 // @match        https://namethatporn.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=namethatporn.com
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.2/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.5.7/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.4/dist/jabroni-outfit.umd.js
 // @run-at       document-idle
 // ==/UserScript==
 
-const { timeToSeconds, sanitizeStr, DataManager, createInfiniteScroller } = window.bhutils;
-const { JabroniOutfitStore, JabroniOutfitUI, defaultSchemeWithPrivacyFilter, defaultStateWithDurationAndPrivacy } = window.jabronioutfit;
+const { getPaginationStrategy, sanitizeStr, DataManager, createInfiniteScroller } = window.bhutils;
+const {
+  JabroniOutfitStore,
+  JabroniOutfitUI,
+  defaultSchemeWithPrivacyFilter,
+  defaultStateWithDurationAndPrivacy,
+} = window.jabronioutfit;
 
 const LOGO = `
 ⡳⣝⢮⡳⣝⢮⡳⣝⢮⢳⡣⣗⡳⡳⡳⣕⢗⣝⢮⡳⡽⣯⡿⣽⢯⡿⣽⡯⣿⢽⢿⢽⢯⢿⢽⢽⣫⢯⢯⢯⢯⢯⢯⣗⢯⣟⢮⢗⢽⡘⡐⣞⣯⢿⡽⣽⣺⣝⢮⡳
@@ -43,52 +48,37 @@ const LOGO = `
 ⢿⣝⢽⣺⣺⡪⡯⡯⣺⢽⣕⢯⡻⡮⡯⣟⣾⢿⣿⢽⢽⢝⡽⡜⣎⢮⢳⢕⢧⢳⢹⡸⣱⢣⡣⡳⣹⡸⡼⡕⣗⢵⡳⡽⣕⣯⡳⣝⡮⣗⡧⣗⡾⡵⣯⣲⡳⡧⡯⢢`;
 
 class NAMETHATPORN_RULES {
-    delay = 350;
+  container = document.querySelector('#items_wrapper, #nsw_r');
 
-    constructor() {
-        this.paginationElement = document.querySelector('#smi_wrp, #nsw_p');
-        this.paginationLast = parseInt([...document.querySelectorAll('#smi_wrp a, #nsw_p a')].pop()?.href.match(/\d+/)?.[0] || 1);
-        Object.assign(this, this.URL_DATA());
-        this.CONTAINER = document.querySelector('#items_wrapper, #nsw_r');
-    }
+  paginationStrategy = getPaginationStrategy({
+    paginationSelector: '#smi_wrp, #nsw_p',
+  });
 
-    IS_PRIVATE(thumb) {
-      return !!thumb.querySelector('.item_solved, .nsw_r_slvd');
-    }
+  isPrivate(thumb) {
+    return !!thumb.querySelector('.item_solved, .nsw_r_slvd');
+  }
 
-    THUMB_URL(thumb) {
-      return thumb.querySelector('a')?.href || thumb.href;
-    }
+  getThumbUrl(thumb) {
+    return thumb.querySelector('a')?.href || thumb.href;
+  }
 
-    GET_THUMBS(html) {
-      return [...html.querySelectorAll('.item, .nsw_r_w')];
-    }
+  getThumbs(html) {
+    return [...html.querySelectorAll('.item, .nsw_r_w')];
+  }
 
-    THUMB_IMG_DATA(thumb) {
-      const img = thumb.querySelector('img');
-      const imgSrc = img.getAttribute('data-dyn')?.concat('.webp') || img.getAttribute('src');
-      return ({ img, imgSrc });
-    }
+  getThumbImgData(thumb) {
+    const img = thumb.querySelector('img');
+    const imgSrc = img.getAttribute('data-dyn')?.concat('.webp') || img.getAttribute('src');
+    return { img, imgSrc };
+  }
 
-    THUMB_DATA(thumb) {
-        const item_answer = sanitizeStr(thumb.querySelector('.item_answer b, .nsw_r_desc')?.innerText);
-        const item_title = sanitizeStr(thumb.querySelector('.item_title, .nsw_r_tit')?.innerText);
-        const title = `${item_title} ${item_answer}`;
-        const duration = 0;
-        return { title, duration };
-    }
-
-    URL_DATA() {
-        const url = new URL(location.href);
-        const paginationOffset = parseInt(url.searchParams.get('page')) || 1;
-
-        const paginationUrlGenerator = n => {
-            url.searchParams.set('page', n);
-            return url.href;
-        }
-
-        return { paginationOffset, paginationUrlGenerator }
-    }
+  getThumbData(thumb) {
+    const item_answer = sanitizeStr(thumb.querySelector('.item_answer b, .nsw_r_desc')?.innerText);
+    const item_title = sanitizeStr(thumb.querySelector('.item_title, .nsw_r_tit')?.innerText);
+    const title = `${item_title} ${item_answer}`;
+    const duration = 0;
+    return { title, duration };
+  }
 }
 
 const RULES = new NAMETHATPORN_RULES();
@@ -96,11 +86,11 @@ const RULES = new NAMETHATPORN_RULES();
 //====================================================================================================
 
 function router() {
-  if (RULES.CONTAINER) {
-      parseData(RULES.CONTAINER);
+  if (RULES.container) {
+    parseData(RULES.container);
   }
 
-  if (RULES.paginationElement) {
+  if (RULES.getPaginationStrategy.hasPagination) {
     createInfiniteScroller(store, parseData, RULES);
   }
 
@@ -111,29 +101,31 @@ function router() {
 
 delete defaultSchemeWithPrivacyFilter.durationFilter;
 defaultSchemeWithPrivacyFilter.privacyFilter = [
-  { type: "checkbox", model: "state.filterPrivate", label: "unsolved" },
-  { type: "checkbox", model: "state.filterPublic", label: "solved" }];
+  { type: 'checkbox', model: 'state.filterPrivate', label: 'unsolved' },
+  { type: 'checkbox', model: 'state.filterPublic', label: 'solved' },
+];
 
 //====================================================================================================
 
 function monkeyPatchConfirm() {
-  const realConfirm = unsafeWindow.confirm;
-  unsafeWindow.confirm = function() {
-    return true;
-  };
+  // const realConfirm = unsafeWindow.confirm;
+  unsafeWindow.confirm = () => true;
 }
 
 monkeyPatchConfirm();
 
-window.addEventListener("keydown", (event) => {
-  if (event.key === 'c') {
-    const name = document.querySelector('#loggedin_box_new_username').innerText;
-    if (!document.querySelector(`.ida_confirm_usernames a[href$="${name}.html"]`)) {
-      document.querySelector('.id_answer_buttons > .iab.iac').click();
+window.addEventListener(
+  'keydown',
+  (event) => {
+    if (event.key === 'c') {
+      const name = document.querySelector('#loggedin_box_new_username').innerText;
+      if (!document.querySelector(`.ida_confirm_usernames a[href$="${name}.html"]`)) {
+        document.querySelector('.id_answer_buttons > .iab.iac').click();
+      }
     }
-  }
-}, { once: true });
-
+  },
+  { once: true },
+);
 
 //====================================================================================================
 
