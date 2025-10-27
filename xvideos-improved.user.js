d163d2c3bc518d17df19569be5850658e3ee2e3f
orangutan <atm.mormon@protonmail.com>
2025-10-27 23:19:54 +0000

    Upgrade to version 3.0.1 and refactor code
    Updated version and dependencies, refactored code for better readability and structure.

--- xvideos-improved.user.js
+++ xvideos-improved.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         XVideos Improved
 // @namespace    http://tampermonkey.net/
-// @version      2.2.0
+// @version      3.0.1
 // @license      MIT
 // @description  Infinite scroll. Filter by duration, include/exclude phrases
 // @author       smartacephale
@@ -9,7 +9,7 @@
 // @match        https://*.xvideos.com/*
 // @exclude      https://*.xvideos.com/embedframe/*
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.4.2/dist/billy-herrington-utils.umd.js
+// @require      https://cdn.jsdelivr.net/npm/billy-herrington-utils@1.5.7/dist/billy-herrington-utils.umd.js
 // @require      https://cdn.jsdelivr.net/npm/jabroni-outfit@1.6.4/dist/jabroni-outfit.umd.js
 // @run-at       document-idle
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=xvideos.com
@@ -17,8 +17,8 @@
 // @updateURL https://update.sleazyfork.org/scripts/494005/XVideos%20Improved.meta.js
 // ==/UserScript==
 
-const { DataManager, createInfiniteScroller, sanitizeStr, timeToSeconds, parseDom } = window.bhutils;
-const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI, DefaultScheme } = window.jabronioutfit;
+const { DataManager, createInfiniteScroller, sanitizeStr, timeToSeconds, parseDom, getPaginationStrategy } = window.bhutils;
+const { JabroniOutfitStore, defaultStateWithDuration, JabroniOutfitUI } = window.jabronioutfit;
 
 const LOGO = `
 ⡐⠠⠀⠠⠐⡀⠆⡐⠢⡐⠢⡁⢆⠡⢂⠱⡈⠔⣈⠒⡌⠰⡈⠔⢢⢁⠒⡰⠐⢢⠐⠄⣂⠐⡀⠄⢂⠰⠀⢆⡐⠢⢐⠰⡀⠒⢄⠢⠐⣀⠂⠄⢀⢂⠒⡰⠐⡂⠔⠂⡔⠂⡔⠂⡔⠂⡔⠂⢆⠡
@@ -49,57 +49,34 @@ const LOGO = `
 ⢁⠎⡐⠆⡥⢃⡌⠓⡬⢑⢢⢃⠎⡜⢢⠜⡱⢌⡱⡘⡤⢣⡙⡜⢦⡹⢜⡪⣝⢪⢳⣙⠶⣳⡹⣬⢻⡱⣏⢾⡱⢯⡞⣵⡻⣝⡮⡽⣎⢷⡹⣎⢷⡹⣎⢷⡹⡖⡽⣒⡻⣌⡳⣍⢖⡣⡝⢦⠹⡜
 ⢊⠬⡑⢎⡰⢡⢊⠵⣈⠇⡎⡜⡸⢌⠣⢎⡱⢊⡴⢣⢱⢣⡹⠜⣦⡙⢮⡱⢎⢧⡳⢭⡞⡵⣳⢭⡳⣝⢮⡳⣏⡷⣹⢧⣻⣜⣳⡝⣮⢳⡻⣜⢧⡻⣜⢧⣛⡽⣜⢣⡗⣥⠳⡜⢮⡑⢮⣑⢫⢜`;
 
-
 class XVIDEOS_RULES {
-    delay = 300;
+  container = document.querySelector('#content')?.firstElementChild;
+  HAS_VIDEOS = !!document.querySelector('div.thumb-block[id^=video_]');
 
-    constructor() {
-        this.paginationElement = [...document.querySelectorAll('.pagination')].pop();
-        this.paginationLast = parseInt(document.querySelector('.last-page')?.innerText) || 1;
-        Object.assign(this, this.URL_DATA());
-        this.CONTAINER = document.querySelector('#content')?.firstElementChild;
-        this.HAS_VIDEOS = !!document.querySelector('div.thumb-block[id^=video_]');
-    }
+  paginationStrategy = getPaginationStrategy({
+    paginationSelector: '#main .pagination:last-child',
+    searchParamSelector: 'p'
+  });
 
-    GET_THUMBS(html) { return html.querySelectorAll('div.thumb-block[id^=video_]:not(.thumb-ad)'); }
+  getThumbs(html) { return html.querySelectorAll('div.thumb-block[id^=video_]:not(.thumb-ad)'); }
 
-    THUMB_IMG_DATA() { return ({}); };
+  getThumbImgData() { return ({}); };
 
-    THUMB_URL(thumb) { return thumb.querySelector('.title a').innerText; }
+  getThumbUrl(thumb) { return thumb.querySelector('.title a').innerText; }
 
-    THUMB_DATA(thumb) {
-      const uploader = sanitizeStr(thumb.querySelector('[class*=name]')?.innerText);
-      const title = sanitizeStr(thumb.querySelector('[class*=title]')?.innerText)
-        .concat(uploader ? ` user:${uploader}` : "");
-      const duration = timeToSeconds(sanitizeStr(thumb.querySelector('[class*=duration]')?.innerText));
+  getThumbData(thumb) {
+    const uploader = sanitizeStr(thumb.querySelector('[class*=name]')?.innerText);
+    const title = sanitizeStr(thumb.querySelector('[class*=title]')?.innerText)
+      .concat(uploader ? ` user:${uploader}` : "");
+    const duration = timeToSeconds(sanitizeStr(thumb.querySelector('[class*=duration]')?.innerText));
 
-      setTimeout(() => {
-          const id = parseInt(thumb.getAttribute('data-id'));
-          unsafeWindow.xv.thumbs.prepareVideo(id);
-      }, 200);
+    setTimeout(() => {
+      const id = parseInt(thumb.getAttribute('data-id'));
+      unsafeWindow.xv.thumbs.prepareVideo(id);
+    }, 200);
 
-      return { title, duration }
-    }
-
-    URL_DATA() {
-        const url = new URL(window.location.href);
-        const paginationOffset = parseInt(url.searchParams.get('p') || url.pathname.match(/\/(\d+)\/?$/)?.pop()) || 0;
-        if (!url.searchParams.get('k')) {
-            if (url.pathname === '/') url.pathname = '/new';
-            if (!/\/(\d+)\/?$/.test(url.pathname)) url.pathname = `${url.pathname}/${paginationOffset}/`;
-        }
-
-        const paginationUrlGenerator = (n) => {
-            if (url.searchParams.get('k')) {
-                url.searchParams.set('p', n);
-            } else {
-                url.pathname = url.pathname.replace(/\/(\d+)\/?$/, `/${n}/`);
-            }
-            return url.href;
-        }
-
-        return { paginationOffset, paginationUrlGenerator }
-    }
+    return { title, duration }
+  }
 }
 
 const RULES = new XVIDEOS_RULES();
@@ -107,60 +84,60 @@ const RULES = new XVIDEOS_RULES();
 //====================================================================================================
 
 function createPreviewElement(src, mount) {
-    const elem = parseDom(`
+  const elem = parseDom(`
     <div class="videopv" style="display: none;">
         <video autoplay="autoplay" playsinline="playsinline" muted="muted"></video>
     </div>`);
 
-    mount.after(elem);
-    const video = elem.querySelector('video');
-    video.src = src;
-    video.addEventListener('loadeddata', () => {
-        mount.style.opacity = '0';
-        elem.style.display = 'block';
-        elem.style.background = '#000';
-    }, false);
+  mount.after(elem);
+  const video = elem.querySelector('video');
+  video.src = src;
+  video.addEventListener('loadeddata', () => {
+    mount.style.opacity = '0';
+    elem.style.display = 'block';
+    elem.style.background = '#000';
+  }, false);
 
-    return {
-        removeElem: () => {
-            video.removeAttribute('src');
-            video.load();
-            elem.remove();
-            mount.style.opacity = '1';
-        }
-    };
+  return {
+    removeElem: () => {
+      video.removeAttribute('src');
+      video.load();
+      elem.remove();
+      mount.style.opacity = '1';
+    }
+  };
 }
 
 function getVideoURL(src) {
-    return src
-        .replace(/thumbs169l{1,}/, 'videopreview')
-        .replace(/\/\w+\.\d+\.\w+/, '_169.mp4')
-        .replace(/(-\d+)_169\.mp4/, (_, b) => `_169${b}.mp4`)
+  return src
+    .replace(/thumbs169l{1,}/, 'videopreview')
+    .replace(/\/\w+\.\d+\.\w+/, '_169.mp4')
+    .replace(/(-\d+)_169\.mp4/, (_, b) => `_169${b}.mp4`)
 }
 
 function animate() {
-    function handleThumbHover(e) {
-        if (!(e.target.tagName === 'IMG' && e.target.id.includes('pic_'))) return;
-        const videoSrc = getVideoURL(e.target.src);
-        const { removeElem } = createPreviewElement(videoSrc, e.target);
-        e.target.parentElement.parentElement.parentElement.addEventListener('mouseleave', removeElem, { once: true });
-    }
+  function handleThumbHover(e) {
+    if (!(e.target.tagName === 'IMG' && e.target.id.includes('pic_'))) return;
+    const videoSrc = getVideoURL(e.target.src);
+    const { removeElem } = createPreviewElement(videoSrc, e.target);
+    e.target.parentElement.parentElement.parentElement.addEventListener('mouseleave', removeElem, { once: true });
+  }
 
-    RULES.CONTAINER.addEventListener('mouseover', handleThumbHover);
+  RULES.container.addEventListener('mouseover', handleThumbHover);
 }
 
 //====================================================================================================
 
 function route() {
-    if (RULES.paginationElement) {
-      createInfiniteScroller(store, parseData, RULES);
-    }
+  if (RULES.paginationStrategy.hasPagination) {
+    createInfiniteScroller(store, parseData, RULES);
+  }
 
-    if (RULES.HAS_VIDEOS) {
-        animate();
-        parseData(RULES.CONTAINER)
-        new JabroniOutfitUI(store);
-    }
+  if (RULES.HAS_VIDEOS) {
+    animate();
+    parseData(RULES.container)
+    new JabroniOutfitUI(store);
+  }
 }
 
 //====================================================================================================
