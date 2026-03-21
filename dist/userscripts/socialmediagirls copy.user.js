e124e5f626899ed5d033fb4128f703350602e843
smartacephale <atm.mormon@protonmail.com>
2026-03-21 02:25:46 +0200

    simpcity

---
+++ dist/userscripts/socialmediagirls copy.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Socialmediagirls PervertMonkey
+// @namespace    pervertmonkey
+// @version      1.0.3
+// @author       violent-orangutan
+// @description  Infinite scroll [optional], Filter by Title
+// @license      MIT
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=socialmediagirls.com
+// @homepage     https://github.com/smartacephale/sleazy-fork#readme
+// @homepageURL  https://sleazyfork.org/en/users/1253342-smartacephale
+// @source       https://github.com/smartacephale
+// @supportURL   https://github.com/smartacephale/sleazy-fork/issues
+// @match        https://forums.socialmediagirls.com/threads/*
+// @require      https://cdn.jsdelivr.net/npm/pervert-monkey@1.0.24/dist/core/pervertmonkey.core.umd.js
+// @require      data:application/javascript,var core = window.pervertmonkey.core || pervertmonkey.core; var utils = core;
+// @grant        GM_addStyle
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
+
+(function (core) {
+  'use strict';
+
+  new core.Rules({
+    containerSelector: ".js-replyNewMessageContainer",
+    paginationStrategyOptions: {
+      paginationSelector: ".block-container + * .pageNav",
+      pathnameSelector: /\/page-(\d+)\/?$/
+    },
+    thumbs: {
+      selector: "article.message"
+    },
+    thumb: {
+      strategy: "auto-text"
+    },
+    gropeStrategy: "all-in-all",
+    schemeOptions: ["Title Filter", "Badge", "Advanced"]
+  });
+
+})(core);
---
+++ src/userscripts/scripts/simpcity.ts
@@ -0,0 +1,26 @@
+import type { MonkeyUserScript } from 'vite-plugin-monkey';
+import { Rules } from '../../core';
+
+export const meta: MonkeyUserScript = {
+  name: 'Simpcity PervertMonkey',
+  version: '1.0.0',
+  description: 'Infinite scroll [optional], Filter by Title',
+  match: 'https://simpcity.cr/threads/*',
+  'run-at': 'document-end',
+};
+
+const rules = new Rules({
+  containerSelector: '.js-replyNewMessageContainer',
+  paginationStrategyOptions: {
+    paginationSelector: '.block-container + * .pageNav',
+    pathnameSelector: /\/page-(\d+)\/?$/,
+  },
+  thumbs: {
+    selector: 'article.message',
+  },
+  thumb: {
+    strategy: 'auto-text',
+  },
+  gropeStrategy: 'all-in-all',
+  schemeOptions: ['Title Filter', 'Badge', 'Advanced'],
+});
